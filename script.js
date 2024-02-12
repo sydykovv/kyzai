586939995e9943bfd5d5e241a8e2eb808db67483
sydykovv <103875694+sydykovv@users.noreply.github.com>
2024-02-13 00:08:51 +0600

    kyzai

---
+++ script.js
@@ -0,0 +1,65 @@
+// пропишем функцию random
+const random = (min, max) => {
+    const rand = min + Math.random() * (max - min + 1);
+    return Math.floor(rand);
+}
+
+// найдем кнопку
+const btn = document.querySelector('#btn');
+// повесим обработчик событий
+btn.addEventListener('mouseenter', () => {
+    btn.style.left = `${random(0, 90)}%`;
+    btn.style.top = `${random(0, 90)}%`;
+});
+
+// в случае победы выведем:
+btn.addEventListener('click', () => {
+    alert('Congrats! You hit the button! What are you doing with your life? :D');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // функция рандом
+// const random = (min, max) => {
+//   const rand = min + Math.random() * (max - min + 1);
+//   return Math.floor(rand);
+// };
+
+// // убегающая кнопка
+// const btn = document.querySelector('#btn');
+// btn.addEventListener('mouseenter', () => {
+//     btn.style.left = `${random(0, 90)}%`;
+//     btn.style.top = `${random(0, 90)}%`;
+// });
+
+// // победа
+// btn.addEventListener('click',
+//   () => alert('Congratulations! You hit the button! What are you doing with your life?')
+// );
